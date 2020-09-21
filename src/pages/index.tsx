b6bb4636675fd05fa86fbc895a1d0e4d12d3c59e
Sam Millar <sam@millar.io>
2020-09-21 22:34:06 +0000

    Fix date string

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Index() {
           <Link href="https://www.subtleradio.com/" color="brand.primary.500">
             the mic
           </Link>{" "}
-          {next.toLocaleString()}
+          {next.toLocaleString(DateTime.DATETIME_MED)}
         </Box>
       </Stack>
     </ThemeProvider>
