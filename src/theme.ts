82c5cda8cbf7677281cfe090b7f96c4dafa282ca
Sam Millar <sam@millar.io>
2020-09-24 20:17:26 +0100

    Clean up theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -2,9 +2,6 @@ import { theme } from "@chakra-ui/core";
 import { merge } from "@chakra-ui/utils";
 
 export default merge(theme, {
-  fonts: {
-    // heading: `${theme.fonts.heading}`,
-  },
   colors: {
     brand: {
       primary: theme.colors.red,
